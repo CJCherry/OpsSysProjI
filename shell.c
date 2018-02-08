e07d3d4f40fdc563c8524138d5000e31c25dc9ee
Colin Cherry <cjc14j@my.fsu.edu>
2018-02-08 15:40:19 -0500

    Add files via upload

--- shell.c
+++ shell.c
@@ -3,6 +3,12 @@
 #include <unistd.h>
 #include <strings.h>
 #include <sys/time.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+void execute(char ** cmd);
+void redirectOutput();
+void redirectInput();
 
 int main()
 {
@@ -33,6 +39,15 @@ int main()
 			return 0;
 		}
 	} while(1);
+	
+	//for testing
+	char programPath[] = "/home/majors/aanderso/COP4610/Proj1/a.out";
+	char * argv[] = {programPath, NULL, NULL, NULL};
+	
+	//test calls
+	//execute(argv);
+	//redirectOutput();
+	//redirectInput();
 
 }
 
@@ -53,4 +68,88 @@ int PrintPrompt()
 {
 	printf("%s@%s :: %s => ", getenv("USER"), getenv("MACHINE"), getenv("PWD"));
 	return 0;
+}
+
+//works
+void execute(char ** cmd) {
+	int status;
+	pid_t pid = fork();
+	if (pid == -1) {
+		printf("err");
+		exit(1);
+	}
+	else if (pid == 0) {
+		//child
+		//cmd[0] is absolute pathname
+		//cmd is arguments
+		//ex: char *cmd[4] = { “/bin/ls”, “-l”, “-a”, NULL };
+		execv(cmd[0], cmd);
+		//if error executing:
+		printf("Problem executing %s\n", cmd[0]);
+		exit(1);
+	}
+	else {
+		//parent
+		//status?
+		//wait until child finishes
+		waitpid(pid, &status, 0);
+	}
+}
+
+//works
+void redirectOutput () {
+	//parse filenames
+	
+	//program to get output from
+	char programPath[] = "/home/majors/aanderso/COP4610/Proj1/a.out";
+	char * argv[] = {programPath, NULL, NULL, NULL};
+	
+	//for testing, file to redirect output to
+	char filePath[] = "/home/majors/aanderso/COP4610/Proj1/output.txt";
+	
+	//file desc table for output.txt
+	//O_WRONLY = write mode
+	//O_CREAT = if file dne, create it
+	int fdSpot = open(filePath, O_WRONLY | O_CREAT);
+	
+	//fork creates another file descr table
+	if (fork() == 0) {
+		//child
+		
+		//close stdout
+		close(STDOUT_FILENO);
+		//fdSpot replaces stdout
+		dup(fdSpot);
+		close(fdSpot);
+		execute(argv);
+	}
+	else {
+		//parent
+		close(fdSpot);
+	}
+}
+
+//works
+void redirectInput () {
+	//for testing, program to redirect input to
+	char programPath[] = "/home/majors/aanderso/COP4610/Proj1/a.out";
+	char * argv[] = {programPath, NULL, NULL, NULL};
+	
+	//for testing, file to get input from
+	char filePath[] = "/home/majors/aanderso/COP4610/Proj1/input.txt";
+	
+	int fdSpot = open(filePath, O_RDONLY);
+	
+	//fork creates another file descr table
+	if (fork() == 0) {
+		//child
+		close(STDIN_FILENO);
+		dup(fdSpot);
+		close(fdSpot);
+		execute(argv);
+	}
+	else {
+		//parent
+		close(fdSpot);
+	}
 }
